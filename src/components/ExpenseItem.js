c0ff8fb20dbb9c7403c42124c80a7c3f23dd74fe
SaikiranUppulanche <suppulanche@gmail.com>
2024-01-11 19:18:32 +0530

    makingDisplayDataDynamic

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,20 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date().toLocaleString();
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 8000;
+  const locationOfExpenditure = "Mumbai";
+
   return (
-    <div>
-      <h2>Expense Item!</h2>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 200</p>
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__description">
+        <h2>
+          {expenseTitle} in {locationOfExpenditure}
+        </h2>
+        <div className="expense-item__price">₹ {expenseAmount}</div>
+      </div>
     </div>
   );
 }
