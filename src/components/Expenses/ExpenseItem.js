3531a68af01cf247b7560b22769a72bdb50c71a8
SaikiranUppulanche <suppulanche@gmail.com>
2024-01-13 10:41:07 +0530

    update amount on clickto

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const handleClick = (event) => {
-    event.target.parentElement.remove();
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+
+  const handleClick = () => {
+    setTitle("Updated!");
+  };
+
+  const handleAmount = () => {
+    setAmount(100);
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+        <button onClick={handleAmount}>Update Amount</button>
       </div>
-      <button onClick={handleClick}>Delete</button>
+      <button onClick={handleClick}>UpdateTitle</button>
     </Card>
   );
 };
