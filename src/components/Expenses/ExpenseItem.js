2115634ccd37d673167ebe1c156a7a083718e3f8
SaikiranUppulanche <suppulanche@gmail.com>
2024-01-13 17:00:01 +0530

    Adding a filter which doesn't work yet

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const [amount, setAmount] = useState(props.amount);
-
-  const handleClick = () => {
-    setTitle("Updated!");
-  };
-
-  const handleAmount = () => {
-    setAmount(100);
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
-        <button onClick={handleAmount}>Update Amount</button>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={handleClick}>UpdateTitle</button>
     </Card>
   );
 };
